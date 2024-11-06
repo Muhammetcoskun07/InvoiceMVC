a8f3100332a06b656462ee63890be0688fdb3b60
Muhammetcoskun07 <mc522848@gmail.com>
2024-11-06 20:37:52 +0300

    Client add

---
+++ Models/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace InvoiceMVC.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public string StreetAddress { get; set; }
+
+     
+    }
+}
